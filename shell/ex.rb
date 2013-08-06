5994651fdeb8dd1530216c93c26957290dc02705
Brandon Holt <bholt@cs.washington.edu>
2013-08-06 15:27:41 -0700

    rb-accel: add 'ex' command that can do filtering and prefixing, backticks uses this

---
+++ shell/ex.rb
@@ -0,0 +1,22 @@
+
+## TODO: finish implementing filters...
+module Accel
+  @@ex_filters = []
+end
+
+def ex(cmd, opts = {prefix:''})
+  require 'open3'
+  out = ''
+  Open3.popen2e(cmd) do |i,oe,waiter|
+    pid = waiter.pid
+    oe.each_line do |l|
+      p = ( block_given? ? yield(l) : l )
+      print "#{opts[:prefix]}#{p}"
+      out << p
+    end
+    # TODO: add filtering capability
+    exit_status = waiter.value
+    raise "shell command error: #{exit_status}" if not exit_status.success?
+  end
+  return out
+end
--- shell/ticks.rb
+++ shell/ticks.rb
@@ -1,14 +1,6 @@
+require_relative 'ex'
 
 # Overrides backtick operator (``) to echo everything to stdout line-by-line
-def `(command)
-  require 'open3'
-  out = ''
-  Open3.popen2e(command) do |i,oe,waiter|
-    pid = waiter.pid
-    oe.each_line{|l| puts l.strip; out << l }
-    # TODO: add filtering capability
-    exit_status = waiter.value
-    raise "shell command error: #{exit_status}" if not exit_status.success?
-  end
-  return out
+def `(cmd)
+  ex(cmd)
 end
